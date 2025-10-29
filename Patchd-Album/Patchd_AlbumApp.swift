f7d9e741fb17c728e085efb28dd0b05f977b2f18
Ricky Payares <rpayaresswe@gmail.com>
2025-10-29 13:36:10 -0400

    Initial Commit

---
+++ Patchd-Album/Patchd_AlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patchd_AlbumApp.swift
+//  Patchd-Album
+//
+//  Created by Ricardo Payares on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Patchd_AlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
